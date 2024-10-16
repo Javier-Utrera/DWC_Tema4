648dc6c29fd680398640b8285a75d5659704512d
Francisco Javier <francisco.rodriguez.garcia.al@iespoligonosur.org>
2024-10-16 21:53:06 +0200

    terminado los dos ejercicios

--- script.js
+++ script.js
@@ -5,9 +5,29 @@ Utiliza el operador spread.
 */
 function numCifras(numero){ 
     let cifras= [...numero];
+    console.log(cifras.length);
     console.log(cifras);
+}
+
+function contador(){ 
+    let mostrar="";
+    for (const i in arguments) {
+        mostrar += arguments[i];
+    }
+    console.log(mostrar);
+}
+function ejecucion(numero){ 
+    console.log(...numero);
+    contador(...numero);
 }
 /* 6
 Hacer un programa que sume todos los parámetros pasados como argumentos de entrada en la llamada. 
 El número de argumentos de entrada es desconocido.
 */
+function suma(param1,param2, ...restParams){ 
+    let resultado="";
+    for (const i in arguments) {
+        resultado += arguments[i];        
+    }
+    console.log(resultado);
+}
