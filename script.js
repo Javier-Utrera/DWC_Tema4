4125c1b605414e99efa3766dea9ac6fe4c2d674d
Francisco Javier Rodriguez <francisco.rodriguez.garcia.al@iespoligonosur.org>
2024-10-16 14:43:09 +0200

    paracasa

---
+++ script.js
@@ -0,0 +1,13 @@
+/* 3
+Realizar un programa que calcule el número de cifras de un número. 
+Deberá implementarse una función numCifras(numero) que devuelva el número de cifras del mismo. 
+Utiliza el operador spread.
+*/
+function numCifras(numero){ 
+    let cifras= [...numero];
+    console.log(cifras);
+}
+/* 6
+Hacer un programa que sume todos los parámetros pasados como argumentos de entrada en la llamada. 
+El número de argumentos de entrada es desconocido.
+*/
